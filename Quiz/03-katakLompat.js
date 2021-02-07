a6a4a939df4417cb71aee4234027c91c02ae22ed
rizkymuhram312 <kikiberem@gmail.com>
2021-02-07 20:41:51 +0700

    mod-day01

--- Quiz/03-katakLompat.js
+++ Quiz/03-katakLompat.js
@@ -1,3 +1,15 @@
 function howManyFrog(x,y,z){
     
+    let lompat = x;
+    let jumLompat = 0;
+
+    while (lompat < z) { //lakukan hitungan lompatan sampai kondisi z atau posisi akhir terpenuhi
+        lompat = lompat + y; 
+        jumLompat++; //menghitung jumlah lompatan
+        
+    }
+    return jumLompat;
+    
 }
+
+console.log(howManyFrog(10,30,120));
